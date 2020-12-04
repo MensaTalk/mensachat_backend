aafeb99679d41e436431868a270f1d1af218ad8b
abteilung6 <moeller.tilman@gmail.com>
2020-12-04 19:54:49 +0100

    let peer join a socket io room

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import http from 'http';
 import cors from 'cors';
 import { InMemoryDB } from './db';
 import { CONNECT, DISCONNECT, JOIN_ROOM, MESSAGE } from './constants';
+import { ActualMessage, JoinMessage } from './types';
 
 const app = express();
 app.use(cors());
@@ -11,28 +12,33 @@ app.options('*', cors());
 app.set('port', process.env.PORT || 9001);
 
 const server = http.createServer(app);
-
 const io = new ioServer(server);
-
 const db = new InMemoryDB();
-
-console.log(db.rooms);
-console.log(db.users);
 
 io.on(CONNECT, function (socket: Socket) {
   console.log(`Client ${socket.id} connected.`);
   const user = db.addUser({ id: '', name: '' }, socket.id);
   if (user === undefined) {
+    console.log(`Client ${socket.id} not able to connect.`);
     socket.disconnect();
   }
-  socket.on(JOIN_ROOM, function (msg: unknown) {
+  socket.on(JOIN_ROOM, function (msg: JoinMessage) {
     console.log(`Client ${socket.id} try to join with ${msg}.`);
+    if (msg.roomId) {
+      const joinAction = db.joinRoom(socket.id, msg.roomId);
+      if (joinAction) {
+        socket.join(msg.roomId.toString());
+        console.log(`Client ${socket.id} joined ${msg.roomId}.`);
+      } else {
+        console.log(`Client ${socket.id} failed to join ${msg.roomId}.`);
+      }
+    }
   });
-
-  socket.on(MESSAGE, function (msg: unknown) {
-    console.log(`Client ${socket.id} send ${msg}.`);
+  socket.on(MESSAGE, function (msg: ActualMessage) {
+    console.log(`Client ${socket.id} send ${msg.payload}.`);
   });
   socket.on(DISCONNECT, function (socket: Socket) {
+    // TODO: socket.id defined?
     console.log(`Client ${socket.id} disconnected.`);
   });
 });
