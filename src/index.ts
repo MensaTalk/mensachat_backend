7727e554877db1d2ddaffe080641c9729eb0e159
abteilung6 <moeller.tilman@gmail.com>
2020-12-04 15:14:33 +0100

    integrate express with socketio server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello');
+import express from 'express';
+import { Server as ioServer } from 'socket.io';
+import http from 'http';
+
+const app = express();
+app.set('port', process.env.PORT || 9001);
+
+const server = http.createServer(app);
+
+const io = new ioServer(server);
+
+io.on('connection', function (socket: any) {
+  console.log('Client connected!');
+  socket.on('msg', function (msg: any) {
+    console.log(msg);
+  });
+});
+
+server.listen(9001, function () {
+  console.log('listening on *:9001');
+});
