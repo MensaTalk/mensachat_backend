17fe43bfc9aca7b0f7f0f6c03bf59c8b7039abaf
abteilung6 <moeller.tilman@gmail.com>
2020-12-06 17:02:35 +0100

    fix constants, lowercase

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const CONNECT = 'CONNECT';
-export const DISCONNECT = 'DISCONNECT';
-export const JOIN_ROOM = 'JOIN_ROOM';
-export const LEAVE_ROOM = 'LEAVE_ROOM';
-export const MESSAGE = 'MESSAGE';
+export const CONNECT = 'connect';
+export const DISCONNECT = 'disconnect';
+export const JOIN_ROOM = 'join_room';
+export const LEAVE_ROOM = 'leave_room';
+export const MESSAGE = 'message';
