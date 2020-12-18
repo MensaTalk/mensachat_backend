0c7390255b3eb5cc2f34363daafe32d6b6c0e5f1
steven-mi <stevenmi@live.de>
2020-12-18 23:19:47 +0100

    add verification of user

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { AuthenticationInterface, MessageInterface, Room } from './types';
 
+const API_VERIFY_USER_URL =
+  'http://mensatalk.herokuapp.com/verifyUserNameWithToken';
 const API_ROOMS_URL = 'http://mensatalk.herokuapp.com/chatrooms';
 const API_MESSAGE_URL = `http://mensatalk.herokuapp.com/chatmessages`;
 const _get_room_messages_url = (roomId): string => {
@@ -18,7 +20,6 @@ export const createToken = async (): Promise<AuthenticationInterface> => {
       },
     },
   );
-  console.log(response);
   return (await response).data;
 };
 
@@ -51,5 +52,23 @@ export const saveRoomMessages = async (
     },
   });
   console.log(`Sent ${messages.length} room message to heroku`);
+  return (await response).data;
+};
+
+export const verifyUserNameWithToken = async (
+  serverToken: string,
+  userToken: string,
+  userName: string,
+): Promise<number> => {
+  const response = axios.post(
+    API_VERIFY_USER_URL,
+    JSON.stringify({ jwtToken: userToken, userName: userName }),
+    {
+      headers: {
+        Authorization: 'Bearer ' + serverToken,
+        'Content-Type': 'application/json',
+      },
+    },
+  );
   return (await response).data;
 };
